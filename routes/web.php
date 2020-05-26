a0725a92a619cf93cc25c1b6c9d8e2add10df00b
EC2 Default User <ec2-user@ip-172-31-20-250.us-east-2.compute.internal>
2020-05-26 10:09:13 +0000

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', 'PostsController@index');
+
+Auth::routes();
+
+// ==========ここから編集する==========
+// コメントアウトする
+// Route::get('/home', 'HomeController@index')->name('home');
+
+// このコードを追加する
+Route::get('/home', 'PostsController@index');
+// ==========ここまで編集する==========
+
+// ユーザ編集画面
+Route::get('/users/edit', 'UsersController@edit');
+
+// ユーザ詳細画面
+Route::get('/users/{user_id}', 'UsersController@show');
+
+// ==========ここから追加する==========
+// 投稿新規画面
+Route::get('/posts/new', 'PostsController@new')->name('new');
+
+// 投稿新規処理
+Route::post('/posts','PostsController@store');
+
+//投稿削除処理
+Route::get('/postsdelete/{post_id}', 'PostsController@destroy');
+
+//いいね処理
+Route::get('/posts/{post_id}/likes', 'LikesController@store');
+
+//いいね取消処理
+Route::get('/likes/{like_id}', 'LikesController@destroy');
+
+//コメント投稿処理
+Route::post('/posts/{comment_id}/comments','CommentsController@store');
+
+//コメント取消処理
+Route::get('/comments/{comment_id}', 'CommentsController@destroy');
